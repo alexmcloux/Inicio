fa3a9b42c6928ffca319b9a3b260e1acd10588d7
alexmcloux <let35400@educastur.es>
2024-02-01 16:22:41 +0100

    Primer Servelet

---
+++ src/main/java/controlador/BibliotecaController.java
@@ -0,0 +1,44 @@
+package controlador;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class BibliotecaController
+ */
+public class BibliotecaController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public BibliotecaController() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Servlet BibliotecaController");
+		RequestDispatcher despachador=null;
+		despachador = request.getServletContext().getRequestDispatcher("/index.jsp");
+		despachador.forward(request, response);
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
