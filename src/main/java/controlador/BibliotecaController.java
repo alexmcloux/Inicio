c882dc4ca02356abefe07faac9eae49d57106324
alexmcloux <let35400@educastur.es>
2024-02-08 16:44:33 +0100

    JDBC

--- src/main/java/controlador/BibliotecaController.java
+++ src/main/java/controlador/BibliotecaController.java
@@ -2,14 +2,20 @@ package controlador;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import modelo.Libro;
+import modelo.LibroDAO;
+
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Servlet implementation class BibliotecaController
  */
+@WebServlet(urlPatterns = {"","/insertar"})
 public class BibliotecaController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -25,10 +31,35 @@ public class BibliotecaController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("Servlet BibliotecaController");
 		RequestDispatcher despachador=null;
-		despachador = request.getServletContext().getRequestDispatcher("/index.jsp");
+		if(request.getServletPath().equals("")) {
+			try {
+				LibroDAO libroDAO = new LibroDAO();
+				ArrayList<Libro> libros;
+				libros = libroDAO.getLibros();
+				request.setAttribute("libros", libros);
+			}catch(RuntimeException  e) {
+				request.setAttribute("error", e.getMessage());
+			}
+			despachador=request.getServletContext().getRequestDispatcher("/index.jsp");
+		}else if(request.getServletPath().equals("/insertar")){
+			try {
+				LibroDAO libroDAO = new LibroDAO();
+				Libro libro= new Libro(Integer.parseInt(request.getParameter("isbn")),
+						request.getParameter("titulo"),request.getParameter("autor"));
+				libroDAO.insertar(libro);
+				request.setAttribute("info","Libro " + libro+" añadido");
+				
+			}catch(NumberFormatException e) {
+				request.setAttribute("ERROR", e.getMessage());
+			}catch(RuntimeException e) {
+				request.setAttribute("error", e.getMessage());
+			}
+			despachador=request.getServletContext().getRequestDispatcher("/");
+		}
 		despachador.forward(request, response);
+		//despachador = request.getServletContext().getRequestDispatcher("/index.jsp");
+		//despachador.forward(request, response);
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		
 	}
